2eb08531b200df5b59d285c0f38611633131578d
tomthecarrot <code@tomthecarrot.com>
2020-10-17 02:15:58 -0400

    Add Twilio, ExpressJS, and basic SMS confirmation

--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,1 +1,41 @@
-console.log("HELLO");
+// BuzzBasket //
+// Sam Roquitte, Cameron Bennett, Thomas Suarez, Noah Weinstein
+// HackGT 7 //
+
+import express from 'express';
+import twilio from 'twilio';
+import { PORT, defaultRecipient, merchantName, twilioAccountSid, twilioAuthToken } from './config/constants';
+
+// Twilio
+const tw = twilio(twilioAccountSid, twilioAuthToken);
+
+// Express
+const exp = express();
+exp.use(express.json());
+
+exp.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+exp.get('/', (req, res) => {
+    res.send("Welcome to BuzzBasket!");
+});
+
+exp.get('/order*', (req, res) => {
+    const itemName: string|undefined = req.url.split('/order/')[1];
+    if (itemName == undefined || itemName.length == 0) {
+        res.send("Please add an item name to order.");
+        return;
+    }
+    res.send(`Ordered ${itemName}! Sending a confirmation text message...`);
+
+    let message: string = `Thanks for ordering ${itemName}! We appreciate your business. - ${merchantName}`;
+    console.log(message);
+    tw.messages.create({
+        body: message,
+        from: '+16086802899',
+        to: defaultRecipient
+    }).then(message => {
+        console.log(message.sid);
+    });
+});
