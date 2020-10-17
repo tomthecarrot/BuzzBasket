e898ec5393018062428f5c710470ca2a47176b7a
tomthecarrot <code@tomthecarrot.com>
2020-10-17 02:15:58 -0400

    Add Twilio, ExpressJS, and basic SMS confirmation

---
+++ consumer/src/config/constants.ts
@@ -0,0 +1,6 @@
+export const PORT: string = process.env.BUZZBASKET_PORT || "4000";
+export const twilioAccountSid: string|undefined = process.env.BUZZBASKET_TWILIO_ACCOUNT_SID;
+export const twilioAuthToken: string|undefined = process.env.BUZZBASKET_TWILIO_AUTH_TOKEN;
+
+export const defaultRecipient: string = process.env.BUZZBASKET_DEFAULT_RECIPIENT || "0";
+export const merchantName: string = process.env.BUZZBASKET_MERCHANT_NAME || "Your friendly local business";
