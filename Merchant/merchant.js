98c8b21107efba5c42ff5f11f43ef0877b267287
Sam Roquitte <samroq@yahoo.com>
2020-10-18 02:16:29 -0400

    Update api woot doot ma goot

--- Merchant/merchant.js
+++ Merchant/merchant.js
@@ -12,21 +12,7 @@ var jsonParser = bodyParser.json()
 const app = express()
 const port = 3000
 
-const Redis = require('ioredis')
-const redis = new Redis()
-
 const silver = require('./silver')
-
-redis.on('connect', async () => {
-  l.log('DB Connected', "INFO")
-
-  let inv = JSON.parse(await redis.get("inv"))
-  if (!Array.isArray(inv)) {
-    l.log("Inv not array, resetting!", "ERROR")
-    await redis.set("inv", JSON.stringify([]))
-    inv = []
-  }
-})
 
 app.get('/', (req, res) => {
   res.send("PONG")
@@ -41,12 +27,7 @@ app.get('/basketplanner', async (req, res) => {
 // Get all possible baskets
 app.get('/baskets', async (req, res) => {
   l.log("baskets", "get")
-  // let out = await ncr.getCatalog()
-  let out = [
-    {itemId: 0, name: "October Spooky Coffee Basket", price: 68.98},
-    {itemId: 1, name: "Coffee Gift Basket", price: 44.99},
-    {itemId: 2, name: "Enthusiast Coffee Basket", price: 55.96}
-  ]
+  let out = await silver.getBaskets()
   res.send(JSON.stringify(out))
 })
 
@@ -55,6 +36,7 @@ app.post('/order', jsonParser, async (req, res) => {
   let itemId = req.body.itemId
   l.log(`order ${itemId}`, "post")
   let out = await silver.placeOrder(itemId)
+  console.log(out)
   res.send(204)
 })
 
--- Merchant/silver.js
+++ Merchant/silver.js
@@ -5,7 +5,7 @@ const axios = require("axios")
 
 const clientId = "AST00004514"
 const clientSecret = "YYtcjaEa7W16H8bUwWNCocJCmeFzwFuxk2T0pDfZBLSVDNLHAffd5cHgsplLO52ipBkfaOifjPp3tSfjmUhZIwff"
-const token = "gAAAAKCg_CwxpQo7zl6gGDhoKp2cduJ9J7f0TifvxbtpFZBYCSk219dDTyuGrbfvug_2DjIb9vT_Ax3jy1y78aeHu_M19Krk5aBevb8fZvIW4iM4_jhqwEgAGlKRk07JKMOILDoGs9IQr3Oq_s7xMK_JYT5OttjGyFGAwrvu4PPQxVK79AAAAIAAAACd3v15yAT3XHJ4e4mbYHd8vn42AA-OhD6TAVDV1bXVS2uKnaVeyimkzfuzarJQcvKKuwH1XCcnTQIzADb4MOFoltwpfuTmFsciccUZrmkGUOB_EVudUF4HgZtFoeDGYKEXyz-cVN-Blhm0kuPYaPrxMBg8e68KxkoK5gu5QbMQ3XA1Iwu_AncA8YpcwhRAvu_YacmmddWsDnpaDnR8yA09qvfEq5YlgOxChzLrtTQXWP_qyda-xV6Jzjtnfr0hiUIvXziZKCzvMue_aOsfm9gpOQctzQJoiIJgJnFXO5ax2aHE9svJXxOa2_xSVxC8R5g"
+const token = "gAAAAJ5GpkO6tPyePWRkfUnKQw4QWM01DCcdMTns-YoqsVv-ZHV7yVmXx37LErOzW0LucPsJtNQtRZJx2LrB3DKbYscqPnkZ7AY1xz7eUIF60KrCNMxm4sXQWS3ctv2nYA8miAg5MpIUbIabb76fLc7qvHrxSARQLgjyz-rCcJA6Be7W9AAAAIAAAAA_KJL4q_sxensuGzXEWXdKdqVOEMwpV3UFaKGxT7_yDPlGexp_lTZRia4nmSUitawe615gb6it3I5E_3B8mjh7f9rJUfEy1wXQ8m8Gl3PG8nP7BfjIFHzM21S3_ZHEAzch5Fy-cZcbcsdZ29v0hUhmpIaNpzfSrlqWxhT4m1HGpoQxdMG9YL7H3_5I9wHZzZ9lhUvIfkb1Ih3gZwJBCRzeU29L5TmlGUvlg-HJAoNuCkrsI0zHcfzCbRehez37gKNO9qTId5gtAf0FY-aFZCx5Gw2twtY02LVyHNsKN7GMIBaA6w7xVxbbDj0f-DXUFnQ"
 
 const headers = {
     Authorization: `Bearer ${token}`,
@@ -15,12 +15,12 @@ const headers = {
 const host = "https://api-reg-apigee.ncrsilverlab.com"
 
 const getInventory = async () => {
+    l.log("Get inv", "info")
     const params = {
         store_number: 1,
         api_store_id: 1
     }
     const inv = await axios.get(`${host}/v2/inventory/count`, { headers: headers, params: params })
-    l.log("Get inv", "info")
     if (!(inv && inv.data && inv.data.Result)) {
         l.log("No inv!", "error")
         console.log(inv)
@@ -36,29 +36,63 @@ const getInventory = async () => {
     return items
 }
 
-// const getItemPrice = async (itemMasterId) => {
-//     const item
-// }
+const getItemPrice = async (itemMasterId) => {
+    l.log("Get item price", "info")
+    const params = {
+        item_master_id: itemMasterId
+    }
+    const item = await axios.get(`${host}/v2/inventory/items/item`, { headers: headers, params: params })
+    if (!item || !item.data || !item.data.Result) {
+        l.log("Unable to get price", "error")
+        return 0
+    }
+    return item.data.Result.RetailPrice
+}
 
 module.exports = {
     recommendBox: async () => {
         const inventory = await getInventory()
+        let nonBaskets = inventory.filter(item => !(item.ItemCategoryId === 110580))
         let yummies = []
-        if (inventory.length == 0) {
+        if (nonBaskets.length == 0) {
             l.log("Inventory len = 0", "warning")
             return []
         } else {
-            while (inventory.length > 0 && yummies.length < 3) {
-                let curr = inventory.shift()
+            while (nonBaskets.length > 0 && yummies.length < 3) {
+                let curr = nonBaskets.shift()
                 yummies.push({
-                    ItemId: curr.ItemId,
-                    name: curr.ItemName
+                    itemId: curr.ItemId,
+                    name: curr.ItemName,
+                    itemMasterId: curr.ItemMasterId
                 })
             }
         }
+        // use get item request to total up the total basket cost
         return {
             items: yummies,
             totalCost: 58.94
         }
+    },
+    placeOrder: async (item) => {
+        l.log("Place order", "info")
+        // const order = {
+        //     "Orders": 
+        // }
+        const res = await axios.post(`${host}/v2/orders`, order, { headers: headers })
+    },
+    getBaskets: async () => {
+        l.log("Get baskets", "info")
+        let inventory = await getInventory()
+        let baskets = inventory.filter(item => item.ItemCategoryId === 110580)
+        let retVal = []
+        for (b of baskets) {
+            retVal.push({
+                name: b.ItemName,
+                itemId: b.ItemId,
+                itemMasterId: b.ItemMasterId,
+                cost: await getItemPrice(b.ItemMasterId)
+            })
+        }
+        return retVal
     }
 }
